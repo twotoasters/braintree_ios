15bdc25424dd784148c55747a8e12f1c158aa367
John Graham <johngraham262@gmail.com>
2013-04-02 12:25:14 -0400

    Left-align checkbox view.

--- braintree/BTPayment/BTPaymentViewController.m
+++ braintree/BTPayment/BTPaymentViewController.m
@@ -100,7 +100,7 @@
     if (self.venmoTouchEnabled && self.client) {
         // Set up the VTCheckboxView view
         checkboxCardView = [self.client checkboxView];
-        [checkboxCardView setOrigin:CGPointMake(-1, 0)]; // -1 for left-side alignment
+        [checkboxCardView setOrigin:CGPointMake(9, 0)]; // 10-1 for left-side alignment
         [checkboxCardView setWidth:300];
         [checkboxCardView setBackgroundColor:[UIColor clearColor]];
         [checkboxCardView setTextColor:[UIColor grayColor]];
